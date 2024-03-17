0e4b1837f33dc1adf26f48a68d504734eb64e1ee
Tan Wei Siang <tan4575@hotmail.com>
2024-03-17 13:11:27 +0800

    Update config file and fixed UI bug.

--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -97,7 +97,7 @@ export default class Projects extends Component {
 						</div>
 						<div className="row">
 							<div className="col-md-12 animate-box">
-								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Back to Top <i className="icon-reload" /></a></p>
 							</div>
 						</div>
 					</div>
