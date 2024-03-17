cbb97686231872e0e4a7b849362c4544ed4336c2
Tan Wei Siang <tan4575@hotmail.com>
2024-03-17 21:44:22 +0800

    Add secret

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Introduction from './routes/introduction'
 import About from './routes/about'
 import Timeline from './routes/timeline'
 import Projects from './routes/projects'
+const apiKey = process.env.REACT_APP_API_KEY;
 class App extends Component {
   render() {
+    console.log(apiKey)
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
